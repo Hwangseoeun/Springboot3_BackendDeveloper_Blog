a12a8de3d8ef1c81aa2196925e3bcc87f36a013b
Hwangseoeun <sunny030921@gmail.com>
2024-07-24 13:00:22 +0900

    블로그 view 생성(thymeleaf 활용)

--- src/main/java/com/blog/personalblog/domain/Article.java
+++ src/main/java/com/blog/personalblog/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,6 +28,15 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    //엔티티가 생성될 때 생성 시간 저장
+    @CreatedDate
+    @Column(name = "Created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     //빌더 패턴으로 객체 생성
     @Builder
---
+++ src/main/java/com/blog/personalblog/dto/ArticleListViewResponse.java
@@ -0,0 +1,19 @@
+package com.blog.personalblog.dto;
+
+import com.blog.personalblog.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+}
